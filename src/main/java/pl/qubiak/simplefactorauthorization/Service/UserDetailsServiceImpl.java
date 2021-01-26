3905574e7faf2dce460cfdfcd2077f8ad25a2e4d
qubiak <kubiak1986@gmail.com>
2021-01-26 13:33:15 +0100

    add user

---
+++ src/main/java/pl/qubiak/simplefactorauthorization/Service/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package pl.qubiak.simplefactorauthorization.Service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import pl.qubiak.simplefactorauthorization.Repo.AppUserRepo;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private AppUserRepo appUserRepo;
+
+    public UserDetailsServiceImpl(AppUserRepo appUserRepo) {
+        this.appUserRepo = appUserRepo;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        // todo throw if not exist
+        return appUserRepo.findByUsername(s).get();
+    }
+}
