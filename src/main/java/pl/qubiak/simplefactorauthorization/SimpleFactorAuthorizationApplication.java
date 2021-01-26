e08c8d72debcbd57e1b556ad47874ad4ea15dcb3
qubiak <kubiak1986@gmail.com>
2021-01-26 12:25:49 +0100

    init

---
+++ src/main/java/pl/qubiak/simplefactorauthorization/SimpleFactorAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package pl.qubiak.simplefactorauthorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleFactorAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleFactorAuthorizationApplication.class, args);
+	}
+
+}
